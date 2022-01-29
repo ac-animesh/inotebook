9acc921cffeab8958683a92a21bbd2d4a2e023ba
ac-animesh <animeshchandra01@gmail.com>
2022-01-29 18:07:28 +0530

    password encrption

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
 const router = express();
 const User = require('../../models/User');
 
@@ -10,16 +11,38 @@ router.post(
     check('email', 'Please enter your valid email ID').isEmail(),
     check('password', 'Password must be more than 8 char').isLength({ min: 8 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     // User validation
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
+    const { name, email, password } = req.body;
 
-    const user = User(req.body);
-    // user.save();
-    res.send(user);
+    let user = await User.findOne({ email });
+
+    if (user) {
+      return res
+        .status(400)
+        .json({ errors: [{ message: 'User is already exists' }] });
+    }
+
+    try {
+      user = new User({
+        name,
+        email,
+        password,
+      });
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      // user.save();
+      res.send(user);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
   }
 );
 
